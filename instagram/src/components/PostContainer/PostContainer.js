fc540a98682fc822164c3f7e2c87a4a145801fc2
J-E-M <javonmccoy22@gmail.com>
2019-04-09 15:52:35 -0500

    day 1 mvp set up, need add alt attributes for warnings, need to add styling

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Post from './Post';
+import './PostStyles.css';
+
+const PostContainer = props => {
+  return (
+
+    <div className="posts-container">
+
+      {props.posts.map(post => <Post key={post.imageUrl} post={post} />)}
+
+    </div>
+
+  );
+};
+
+export default PostContainer;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar-container">
+
+      <div>
+
+        <input type="text" placeholder="Search..." />
+
+      </div>
+
+    </div>
+  );
+};
+
+export default SearchBar;
